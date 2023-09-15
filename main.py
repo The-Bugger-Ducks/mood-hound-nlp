c5a247dbbfa51762d960f9e61a389e89d9047967
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-09-14 23:01:17 -0300

    feat: update libs, remove coluns and use functions

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
-# =============================================================================
-# Importação de bibliotecas
-# =============================================================================
+import os
+import sys
+utils_path = os.path.dirname(os.path.realpath(__file__)) + '\\utils'
+sys.path.insert(0, utils_path)
+
+import print_topic
 import pandas as pd
 pd.set_option('max_colwidth', 400)
 import numpy as np
 import re
 from unidecode import unidecode
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, log_loss,confusion_matrix
-
 import spacy
 import nltk
 from nltk import FreqDist
-
-import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
-#plt.style.use('seaborn')
-import seaborn as sns
-# %matplotlib inline
-
 import  warnings
 warnings.filterwarnings("ignore")
 
 # =============================================================================
 # Acessando os dados disponibilizados
 # =============================================================================
+print_topic.init('Acessando os dados disponibilizados...')
+
 url = 'https://raw.githubusercontent.com/americanas-tech/b2w-reviews01/main/B2W-Reviews01.csv'
 df = pd.read_csv(url, sep = ',')
-# df.head(5)
+
+print_topic.finish_default()
 
 # =============================================================================
 # Análise exploratória e adaptações
 # =============================================================================
+print_topic.init('Análise exploratória e adaptações...')
+
 df_after = df
 df_after = df_after[~df_after['review_text'].isna()].reset_index(drop=True)
 df_after = df_after[df_after['review_text'].str.contains("\w")]
 df_after = df_after[df_after['review_text'].str.len() > 3]
 df_after = df_after.drop_duplicates('review_text').reset_index(drop=True)
 df_after['submission_date'] = pd.to_datetime(df_after['submission_date']).dt.strftime('%d/%m/%Y')
+df_after = df_after.drop(columns=[
+    'reviewer_id', 
+    'product_brand', 
+    'site_category_lv1', 
+    'site_category_lv2', 
+    'review_title',
+    'recommend_to_a_friend',
+    'reviewer_birth_year',
+    'reviewer_gender'])
 
 analysis_table = {
     'Formato do dataset': [df.shape, df_after.shape], 
@@ -50,14 +53,12 @@ analysis_table = {
 }
 analysis_table = pd.DataFrame(analysis_table)
 analysis_table.index = ['Antes das adaptações', 'Depois das adaptações']
-print('=============================================================================')
-print('Análise exploratória e adaptações')
-print('=============================================================================')
-print(analysis_table)
 
+print_topic.finish_variation()
+print(analysis_table)
+print('-----------------------------------------------------------------------------')
 # Dados depois das adaptações
 df = df_after
-print('=============================================================================')
 print('Dados depois das adaptações')
-print('=============================================================================')
 print(df.head(5))
+print('=============================================================================')
