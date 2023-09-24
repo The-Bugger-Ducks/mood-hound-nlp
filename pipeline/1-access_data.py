b4756b5481abb61a096f999dea8ac98ad6bafb52
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-09-24 19:35:05 -0300

    feat: add 'access data' step

---
+++ pipeline/1-access_data.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# Acessando os dados disponibilizados
+# =============================================================================
+import pandas as pd
+
+def access_data():
+  url = 'https://raw.githubusercontent.com/americanas-tech/b2w-reviews01/main/B2W-Reviews01.csv'
+  df = pd.read_csv(url, sep = ',',low_memory=False)
+  
+  return df
