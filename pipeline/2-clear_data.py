8af53548dbc079f93dc3f74082f5070c5fd700f2
JoaoM-py <joao.marcos2002@hotmail.com>
2023-09-24 22:41:37 -0300

    chore: Remove cluster URL

--- pipeline/2-clear_data.py
+++ pipeline/2-clear_data.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def clear_data(df): 
   df_after = df
   df_after.rename(columns = {'overall_rating': 'sentiment','submission_date':'created_at','review_text':'text'}, inplace=True)
-  df_after['added_at'] = str(datetime.now())
+  df_after['added_at'] = str(datetime.now().isoformat())
   df_after = df_after[~df_after['text'].isna()].reset_index(drop=True)
   df_after = df_after[df_after['text'].str.contains("\w")]
   df_after = df_after[df_after['text'].str.len() > 3]
