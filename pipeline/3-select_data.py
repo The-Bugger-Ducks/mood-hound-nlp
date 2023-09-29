c1f2246aeb1f4eea890d85c6d89a9abd7c77d455
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-09-28 21:06:36 -0300

    refactor: capitalize sentiments

--- pipeline/3-select_data.py
+++ pipeline/3-select_data.py
@@ -10,15 +10,15 @@ def select_data(df):
   negative = df[df['sentiment'] <= 2] # 1 e 2 "estrelas"
 
   df = pd.concat([positive, neutral, negative])
-  ratings_maping = {5: 'Positivo',4: 'Positivo', 3: 'Neutro', 2: 'Negativo', 1: 'Negativo'}
+  ratings_maping = {5: 'POSITIVO',4: 'POSITIVO', 3: 'NEUTRO', 2: 'NEGATIVO', 1: 'NEGATIVO'}
   df = df.replace(ratings_maping)
 
   np.random.seed(seed=100)
-  positive = np.random.choice(df['text'][df['sentiment'] == 'Positivo'], 10000, replace=False)
+  positive = np.random.choice(df['text'][df['sentiment'] == 'POSITIVO'], 10000, replace=False)
   np.random.seed(seed=100)
-  neutral = np.random.choice(df['text'][df['sentiment'] == 'Neutro'], 10000, replace=False)
+  neutral = np.random.choice(df['text'][df['sentiment'] == 'NEUTRO'], 10000, replace=False)
   np.random.seed(seed=100)
-  negative = np.random.choice(df['text'][df['sentiment'] == 'Negativo'], 10000, replace=False)
+  negative = np.random.choice(df['text'][df['sentiment'] == 'NEGATIVO'], 10000, replace=False)
   select = np.concatenate([positive, neutral, negative])
 
   df = df[df['text'].isin(select)].reset_index(drop=True)
