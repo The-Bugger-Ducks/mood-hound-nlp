151f01b509d060bfe1dc2e0e695ad7ba9654be79
JoaoM-py <joao.marcos2002@hotmail.com>
2023-10-16 00:22:56 -0300

    chore: update tests

--- main_tests.py
+++ main_tests.py
@@ -4,7 +4,7 @@ import unittest
 import coverage
 
 
-tests_path = os.path.dirname(os.path.realpath(__file__)) + './tests'
+tests_path = os.path.dirname(os.path.realpath(__file__)) + '/tests'
 sys.path.insert(0, tests_path)
 
 cov = coverage.Coverage()
