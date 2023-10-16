7574fc10c071453a6b58df7b18bcb7f58c681ee1
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-10-15 21:14:26 -0300

    feat: training, test and apply classification model

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ results = pipeline.access_data()
 # Análise exploratória e adaptações
 results = pipeline.clear_data(results)
 
+# Treinando modelo de análise de sentimento
+# pipeline.training_classification_model()
+
 # Selecionando dados a serem utilizados na classificação
 results = pipeline.select_data(results)
 
 # Processamento dos dados
-# results = pipeline.processing(results)
+results = pipeline.processing(results)
 
 # Visualização dos resultados
-# pipeline.show_results(results)
+pipeline.show_results(results)
 
 # Armazenamento dos dados
 # results = pipeline.storage_data(results)
