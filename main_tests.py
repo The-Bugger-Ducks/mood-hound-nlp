c27faffa6e53cd17a60358a4d288521b688da4ab
JoaoM-py <joao.marcos2002@hotmail.com>
2023-10-15 22:05:54 -0300

    feat: Coverage and sonarcloud config

---
+++ main_tests.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import unittest
+import coverage
+
+
+tests_path = os.path.dirname(os.path.realpath(__file__)) + './tests'
+sys.path.insert(0, tests_path)
+
+cov = coverage.Coverage()
+cov.start()
+
+
+loader = unittest.TestLoader()
+suite = loader.discover(tests_path)
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
+
+cov.stop()
+
+cov.xml_report(outfile='coverage.xml')
+
+
+
+
+
+
+
