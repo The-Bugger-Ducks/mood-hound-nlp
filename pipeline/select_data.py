76c87fe1dc2589f2dca27fe205baea8184ffec95
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-11-08 17:30:27 -0300

    feat: increment reviews quantity

--- pipeline/select_data.py
+++ pipeline/select_data.py
@@ -10,9 +10,9 @@ sys.path.insert(0, utils_path)
 import random_select
 
 def select_data(df):
-  positive = random_select.select(100, df, 'POSITIVO', 100)
-  neutral = random_select.select(100, df, 'NEUTRO', 100)
-  negative = random_select.select(100, df, 'NEGATIVO', 100)
+  positive = random_select.select(100, df, 'POSITIVO', 28000) # máximo: 78210
+  neutral = random_select.select(100, df, 'NEUTRO', 15834) # máximo de neutras
+  negative = random_select.select(100, df, 'NEGATIVO', 28000) # máximo: 32594
   select = np.concatenate([positive, neutral, negative])
 
   df = df[df['text'].isin(select)].reset_index(drop=True)
